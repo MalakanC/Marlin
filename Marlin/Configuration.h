d769f2312f7b43b11a1d799115b06c09f2fbd3ab
MalakanC <39969354+MalakanC@users.noreply.github.com>
2021-01-08 23:26:30 +0000

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0 }  // mgc C from 10,10,0 for BLTouch leveling
+#define NOZZLE_TO_PROBE_OFFSET { -44.5, -6, 0 }  // mgc C from 10,10,0 for BLTouch leveling
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
