9164cd51352b37304535b30ad129b13252dd672f
MalakanC <39969354+MalakanC@users.noreply.github.com>
2021-01-09 19:52:06 +0000

    bltouch settings changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44.5, -6, 0 }  // mgc C from 10,10,0 for BLTouch leveling
+#define NOZZLE_TO_PROBE_OFFSET { -44.5, -5, -2 }  // mgc C from 10,10,0 for BLTouch leveling
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1281,7 +1281,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3 // mgc C from 3 for BLTouch leveling
+  #define GRID_MAX_POINTS_X 7 // mgc C from 3 for BLTouch leveling
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -397,7 +397,7 @@
  */
 #define USE_CONTROLLER_FAN  // mgc E
 #if ENABLED(USE_CONTROLLER_FAN)
-  #define CONTROLLER_FAN_PIN 27 // mgc E&C from -1        // Set a custom pin for the controller fan
+  #define CONTROLLER_FAN_PIN PC7 // mgc E&C from -1        // Set a custom pin for the controller fan
   //#define CONTROLLER_FAN_USE_Z_ONLY    // With this option only the Z axis is considered
   //#define CONTROLLER_FAN_IGNORE_Z      // Ignore Z stepper. Useful when stepper timeout is disabled.
   #define CONTROLLERFAN_SPEED_MIN      0 // (0-255) Minimum speed. (If set below this value the fan is turned off.)
